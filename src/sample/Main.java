3462b1e7809cc0137f5eeeb39433eb8b750c47d1
harutiro <harutiro2027@outlook.jp>
2021-05-08 02:02:22 +0900

    testCommit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //アプリケーションがスタートするときの処理
+    //stage = トップレベルコンテナ　GUIの組み込みの土台となるコンテナ
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+        //画面の表示
+        //画面の非表示「hide」
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        //mainが受け取ったStringによりjavaFxを起動させる
         launch(args);
     }
 }
